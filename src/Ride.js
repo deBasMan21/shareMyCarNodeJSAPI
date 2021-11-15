0a3b2902e0482d31d85c1fcbf65f70ac5cb598ce
Bas <bbuijsen@gmail.com>
2021-11-15 19:58:15 +0100

    added schemas for mongodb

---
+++ src/Ride.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RideSchema = new Schema({
+    name: String,
+    beginDateTime: Date,
+    endDateTime: Date,
+    destination: {
+        name: String,
+        address: String,
+        zipCode: String,
+        city: String
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref = 'user'
+    },
+    reservationDateTime: Date
+});
+
+const Ride = mongoose.model('ride', RideSchema);
+
+module.exports = Ride;
---
+++ src/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: String,
+    email: String,
+    phoneNumber: String,
+    cars: [{
+        type: Schema.Types.ObjectId,
+        ref: 'car'
+    }]
+});
+
+const User = mongoose.Schema('user', UserSchema);
+
+module.exports = User;
