cfcdf6348e2b0af891d7dba32e8ed4d6b7bb520a
Bas <bbuijsen@gmail.com>
2021-11-18 15:27:26 +0100

    changed logs

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const app = require('./app');
 
 app.listen(process.env.PORT || 4444, () => {
-    console.log('hello world!');
-    console.log('running app on port 3050');
+    console.log(`running app on port ${process.env.PORT || 4444}`);
 })
